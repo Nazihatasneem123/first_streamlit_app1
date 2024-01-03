45bedea3f82f7fcf62c7b61834104abf0b2e6296
Nazihatasneem123 <147566863+Nazihatasneem123@users.noreply.github.com>
2024-01-03 12:29:42 +1100

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -2,3 +2,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header ('Breakfats Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe my_fruit_list
