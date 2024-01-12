03b5a1e983df01516314cd0c1d7fd4dfc0acd4cc
Nazihatasneem123 <147566863+Nazihatasneem123@users.noreply.github.com>
2024-01-12 21:44:27 +1100

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,9 +1,9 @@
-"""import streamlit
+ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")"""
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # pre populate the list  
