65358b299467ee6c3a491c345ad5868e0b3e8a93
Nazihatasneem123 <147566863+Nazihatasneem123@users.noreply.github.com>
2024-01-12 11:27:38 +1100

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -10,7 +10,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-#------------------------------------------------------------------------------------
+ 
 #reading from api --> normalizing it --> displaying it in the data frame/table format
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
